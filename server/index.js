99ee9d7e858183d4ba4fbf48417ff9f4ac1f3743
Karan <Karandp505@gmail.com>
2024-02-19 23:26:33 +0530

    try again

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ const dbConnection=require("./src/config/dbconnection")
  dbConnection()
  const PORT=process.env.PORT ||5000
 
- app.use(cors())
+ app.use(cors(
+   {
+    origin:["https://btas-pharma-server.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+   }
+ ));
  app.use(express.json())
 //  app.use(FileUpload({
 //     useTempFiles:true
